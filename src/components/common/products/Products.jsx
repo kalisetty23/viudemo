12685242cc301736c29f5ed03c6b6ae4bdb5e2f5
Mani.Teja <mani.teja@ibsplc.com>
2021-05-17 18:57:54 +0530

    Formatting Changes

--- src/components/common/products/Products.jsx
+++ src/components/common/products/Products.jsx
@@ -1,32 +1,19 @@
-import React from 'react';
-import './Products.css';
+import React from "react";
+import "./Products.css";
 
-import { Card } from 'react-bootstrap';
+import { Card } from "react-bootstrap";
 
-
-function Products () {
-    return(
-
-        <div className="App">
-              <Card>
-              
-            
-                  <Card.Body>
-                    <Card.Title>
-                      maintain s
-
-                    </Card.Title>
-                    <Card.Text>
-                      yes  this is card test
-                    </Card.Text>
-                    
-                  </Card.Body>
-                
-              </Card>
-      
-    </div>
-
-    )
+function Products() {
+ return (
+  <div className="App">
+   <Card>
+    <Card.Body>
+     <Card.Title>maintain s</Card.Title>
+     <Card.Text>yes this is card test</Card.Text>
+    </Card.Body>
+   </Card>
+  </div>
+ );
 }
 
 export default Products;
