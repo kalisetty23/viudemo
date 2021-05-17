33d495e950efd1ddcfe2e24caf57fb446fd76e56
Mani.Teja <mani.teja@ibsplc.com>
2021-05-17 19:11:25 +0530

    Replaced anchor tags with Link tags for navigation

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./Navbar.css";
 import { MdTrendingDown } from "react-icons/md";
 import { FiUsers, FiSliders, FiShoppingBag } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 import {
  RiPulseFill,
@@ -15,33 +16,33 @@ import {
 function Navbar() {
  return (
   <section className="navbar">
-   <a href="/" className="navbar-item">
+   <Link to="/" className="navbar-item">
     <RiPulseFill size="1.3em" /> Dashboard
-   </a>
-   <a href="/about" className="navbar-item">
+   </Link>
+   <Link to="/about" className="navbar-item">
     <RiShoppingCartLine size="1.3em" /> Point Of sale
-   </a>
-   <a href="/portfolio" className="navbar-item">
+   </Link>
+   <Link to="/portfolio" className="navbar-item">
     <RiFileTextLine size="1.3em" /> Invoice
-   </a>
-   <a href="/shop" className="navbar-item">
+   </Link>
+   <Link to="/shop" className="navbar-item">
     <FiShoppingBag size="1.3em" /> Orders
-   </a>
-   <a href="/blog" className="navbar-item">
+   </Link>
+   <Link to="/blog" className="navbar-item">
     <FiUsers SIZE="1.3EM" /> Customers
-   </a>
-   <a href="/contact" className="navbar-item">
+   </Link>
+   <Link to="/contact" className="navbar-item">
     <FiSliders size="1.3em" /> Reports
-   </a>
-   <a href="/Products" className="navbar-item">
+   </Link>
+   <Link to="/products" className="navbar-item">
     <RiGiftLine size="1.3em" /> Products
-   </a>
-   <a href="/blog" className="navbar-item">
+   </Link>
+   <Link to="/blog" className="navbar-item">
     <RiSettings2Line size="1.3em" /> Settings
-   </a>
-   <a href="/contact" className="navbar-item">
+   </Link>
+   <Link to="/contact" className="navbar-item">
     <MdTrendingDown size="1.3em" /> Returns
-   </a>
+   </Link>
   </section>
  );
 }
