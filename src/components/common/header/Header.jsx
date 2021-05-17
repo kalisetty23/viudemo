3ec39af7799ea8929c040649c2f5846d71164a7c
Mani.Teja <mani.teja@ibsplc.com>
2021-05-17 18:52:35 +0530

    Formatting Changes and Image tag correction

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,48 +1,27 @@
-import React from 'react';
-import { Navbar } from '../../common';
+import React from "react";
+import { Navbar } from "../../common";
 
-
-import './Header.css';
+import "./Header.css";
 
 function Header() {
-    return (
-
-
-        <
-        section className = "header" >
-        <
-        section className = "header-top" >
-        <section className = "header-top__logo">
-         <a href = "/logo.png" className = "header-logo" > <img src = "/logo.png" className = "header-logos" alt = "logo" > imggggg </img>  </a>
-        </section>
-
-        <
-        section className = "header-top__navbar" >
-        <
-        section className = "header-top__navigation" >
-        <
-        Navbar / >
-
-
-        <
-        /section>  <
-        hr className = "header-top__seperator" / >
-        <
-        /section>  < /
-        section >
-
-        <
-        section className = "header-bottom" >
-
-        <
-        /section> < /
-        section >
-
-
-
-    )
-
-
+ return (
+  <section className="header">
+   <section className="header-top">
+    <section className="header-top__logo">
+     <a href="/logo.png" className="header-logo">
+      <img src="/logo.png" className="header-logos" alt="logo" />
+     </a>
+    </section>
+    <section className="header-top__navbar">
+     <section className="header-top__navigation">
+      <Navbar />
+     </section>
+     <hr className="header-top__seperator" />
+    </section>
+   </section>
+   <section className="header-bottom"></section>
+  </section>
+ );
 }
 
 export default Header;
